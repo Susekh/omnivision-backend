68211a2a0de1c955e8205b58dd5b4789dce2e8b2
Subh <subhranshukhilar@outlook.com>
2026-03-17 01:29:44 +0530

    Extended CORS allowedHeaders to include x-groundstaff-id

--- billion-eye-backend-suraj/app.js
+++ billion-eye-backend-suraj/app.js
@@ -43,7 +43,7 @@ app.use(
     origin: allowedOrigins,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Groundstaff-Id"],
   })
 );
 app.options("*", cors());
